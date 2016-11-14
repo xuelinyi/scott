1200d26116ac2b26c4c2c4a5cba785f638b12a15
xuelinyi <2623152306@qq.com>
2016-11-14 16:29:50 +0800

    hadoop demo in 80.133 test success

--- src/main/java/com/comverse/timesheet/web/util/HUtils.java
+++ src/main/java/com/comverse/timesheet/web/util/HUtils.java
@@ -4,12 +4,15 @@
 package com.comverse.timesheet.web.util;
 
 import java.io.IOException;
+import java.net.URI;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.LocatedFileStatus;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.fs.RemoteIterator;
+import org.apache.hadoop.io.IOUtils;
 
 /**
  * Hadoop 工具类
@@ -75,7 +78,80 @@ public class HUtils {
 		
 		return buff.toString();
 	}
+	
+	 public static void copyFile(Configuration conf ,  String local, String remote) throws IOException {
+		 FileSystem fs = FileSystem.get(URI.create("hdfs://192.168.80.133:9000/"), conf);  
+		 fs.copyFromLocalFile(new Path(local), new Path(remote));  
+	        System.out.println("copy from: " + local + " to " + remote);  
+	    }  
+	  /** 
+	     * 创建文件夹 
+	     * @param conf 
+	     * @param uri 
+	     * @param remoteFile 
+	     * @throws IOException 
+	     */  
+	    public static void markDir(Configuration conf , String uri , String remoteFile ) throws IOException{  
+	        FileSystem fs = FileSystem.get(URI.create(uri), conf);  
+	        Path path = new Path(remoteFile);  
+	          
+	        fs.mkdirs(path);  
+	        System.out.println("创建文件夹"+remoteFile);  
+	           
+	    } 
+	    /** 
+	     * 查看文件 
+	     * @param conf 
+	     * @param uri 
+	     * @param remoteFile 
+	     * @throws IOException 
+	     */  
+	     public static void cat(Configuration conf , String uri ,String remoteFile) throws IOException {  
+	            Path path = new Path(remoteFile);  
+	            FileSystem fs = FileSystem.get(URI.create(uri), conf);  
+	            FSDataInputStream fsdis = null;  
+	            System.out.println("cat: " + remoteFile);  
+	            try {  
+	                fsdis = fs.open(path);  
+	                IOUtils.copyBytes(fsdis, System.out, 4096, false);  
+	            } finally {  
+	                IOUtils.closeStream(fsdis);  
+	                fs.close();  
+	            }  
+	    }  
+	     /** 
+	      * 下载 hdfs上的文件 
+	      * @param conf 
+	      * @param uri 
+	      * @param remote 
+	      * @param local 
+	      * @throws IOException 
+	      */  
+	     public static void download(Configuration conf , String uri ,String remote, String local) throws IOException {  
+	            Path path = new Path(remote);  
+	            FileSystem fs = FileSystem.get(URI.create(uri), conf);  
+	            fs.copyToLocalFile(false,path, new Path(local),true); 
+	            System.out.println("download: from" + remote + " to " + local);  
+	    }  
+	     /** 
+	      * 删除文件或者文件夹 
+	      * @param conf 
+	      * @param uri 
+	      * @param filePath 
+	      * @throws IOException 
+	      */  
+	     public static void delete(Configuration conf , String uri,String filePath) throws IOException {  
+	            Path path = new Path(filePath);  
+	            FileSystem fs = FileSystem.get(URI.create(uri), conf);  
+	            fs.deleteOnExit(path);  
+	            System.out.println("Delete: " + filePath);  
+	    } 
 	public static void main(String[] args) throws IOException {
-		System.out.println(getHdfsFiles("/", true));
+		delete(getConf(), "hdfs://192.168.80.133:9000/", "hdfs://192.168.80.133:9000/tmp/tml_base.sql");
+		download(getConf(),  "hdfs://192.168.80.133:9000/", "hdfs://192.168.80.133:9000/tmp/tml_base.sql", "D:/dsp/dsp code/RetailerInfoSepe/scott.sql");
+		cat(getConf(), "hdfs://192.168.80.133:9000/", "hdfs://192.168.80.133:9000/tmp/tml_base.sql");
+		markDir(getConf(), "hdfs://192.168.80.133:9000/", "hdfs://192.168.80.133:9000/tmp10/xuelinyi");
+		copyFile(getConf(), "D:/dsp/dsp code/RetailerInfoSepe/tml_base.sql", "hdfs://192.168.80.133:9000/tmp");
+		System.out.println("hdfs上的文件有："+getHdfsFiles("/", true));
 	}
 }
