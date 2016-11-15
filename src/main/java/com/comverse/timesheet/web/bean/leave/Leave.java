25d406bc8f4004f931250eff714a3f170ac5c883
xuelinyi <2623152306@qq.com>
2016-11-15 18:27:46 +0800

    test

---
+++ src/main/java/com/comverse/timesheet/web/bean/leave/Leave.java
@@ -0,0 +1,154 @@
+package com.comverse.timesheet.web.bean.leave;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Map;
+
+import org.activiti.bpmn.model.Task;
+import org.activiti.engine.history.HistoricProcessInstance;
+import org.activiti.engine.repository.ProcessDefinition;
+import org.activiti.engine.runtime.ProcessInstance;
+
+public class Leave implements Serializable {
+
+	private static final long serialVersionUID = 3639928826696478170L;
+	private int id;
+	private String processInstanceId;
+	private String userId;
+	private Date startTime;
+	private Date endTime;
+	private Date realityStartTime;
+	private Date realityEndTime;
+	private Date applyTime;
+	private String leaveType;
+	private String reason;
+	
+	private Task task;
+	private Map<String, Object> variables;
+	private ProcessInstance processInstance;
+	private HistoricProcessInstance historicProcessInstance;
+	
+	private ProcessDefinition processDefinition;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getProcessInstanceId() {
+		return processInstanceId;
+	}
+
+	public void setProcessInstanceId(String processInstanceId) {
+		this.processInstanceId = processInstanceId;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public Date getStartTime() {
+		return startTime;
+	}
+
+	public void setStartTime(Date startTime) {
+		this.startTime = startTime;
+	}
+
+	public Date getEndTime() {
+		return endTime;
+	}
+
+	public void setEndTime(Date endTime) {
+		this.endTime = endTime;
+	}
+
+	public Date getRealityStartTime() {
+		return realityStartTime;
+	}
+
+	public void setRealityStartTime(Date realityStartTime) {
+		this.realityStartTime = realityStartTime;
+	}
+
+	public Date getRealityEndTime() {
+		return realityEndTime;
+	}
+
+	public void setRealityEndTime(Date realityEndTime) {
+		this.realityEndTime = realityEndTime;
+	}
+
+	public Date getApplyTime() {
+		return applyTime;
+	}
+
+	public void setApplyTime(Date applyTime) {
+		this.applyTime = applyTime;
+	}
+
+	public String getLeaveType() {
+		return leaveType;
+	}
+
+	public void setLeaveType(String leaveType) {
+		this.leaveType = leaveType;
+	}
+
+	public String getReason() {
+		return reason;
+	}
+
+	public void setReason(String reason) {
+		this.reason = reason;
+	}
+
+	public Task getTask() {
+		return task;
+	}
+
+	public void setTask(Task task) {
+		this.task = task;
+	}
+
+	public Map<String, Object> getVariables() {
+		return variables;
+	}
+
+	public void setVariables(Map<String, Object> variables) {
+		this.variables = variables;
+	}
+
+	public ProcessInstance getProcessInstance() {
+		return processInstance;
+	}
+
+	public void setProcessInstance(ProcessInstance processInstance) {
+		this.processInstance = processInstance;
+	}
+
+	public HistoricProcessInstance getHistoricProcessInstance() {
+		return historicProcessInstance;
+	}
+
+	public void setHistoricProcessInstance(HistoricProcessInstance historicProcessInstance) {
+		this.historicProcessInstance = historicProcessInstance;
+	}
+
+	public ProcessDefinition getProcessDefinition() {
+		return processDefinition;
+	}
+
+	public void setProcessDefinition(ProcessDefinition processDefinition) {
+		this.processDefinition = processDefinition;
+	}
+	
+	
+}
---
+++ src/main/java/com/comverse/timesheet/web/controller/LeaveController.java
@@ -0,0 +1,12 @@
+package com.comverse.timesheet.web.controller;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value="oa/leave")
+public class LeaveController {
+	private static final Logger log = Logger.getLogger(LeaveController.class);
+	
+}
